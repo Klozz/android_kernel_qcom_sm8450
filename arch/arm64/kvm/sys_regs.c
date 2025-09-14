21b33ecd8f9009ecf05cd4d1df195a95f3f45991
klozz <carlosj@klozz.dev>
2025-09-14 04:49:35 -0600

    arm64: kvm: Initialize clidr struct to fix uninitialized  const pointer warning
    The variable 'clidr' of type 'struct sys_reg_desc' was passed uninitialized to get_clidr_el1(),
    triggering a -Wuninitialized-const-pointer compiler error.
    
    Explicitly initialize the 'clidr' struct to zero ({0}) upon declaration.
    This ensures all its members (including .val) are properly initialized before their address is taken and passed to the get_clidr_el1() function.
    
    Fixes: This fix build issues on clang-22
    
    Change-Id: Iab733e476fc2443611d90dd298ff3a8818cc3787
    Signed-off-by: klozz <carlosj@klozz.dev>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -2750,7 +2750,7 @@ int kvm_arm_copy_sys_reg_indices(struct kvm_vcpu *vcpu, u64 __user *uindices)
 void kvm_sys_reg_table_init(void)
 {
 	unsigned int i;
-	struct sys_reg_desc clidr;
+ 	struct sys_reg_desc clidr = {0}; // Initialize the struct
 
 	/* Make sure tables are unique and in order. */
 	BUG_ON(check_sysreg_table(sys_reg_descs, ARRAY_SIZE(sys_reg_descs), false));
@@ -2774,7 +2774,7 @@ void kvm_sys_reg_table_init(void)
 	 *   value of 0b000, the values of Ctype4 to Ctype7 must be
 	 *   ignored.
 	 */
-	get_clidr_el1(NULL, &clidr); /* Ugly... */
+	get_clidr_el1(NULL, &clidr); /* Now clidr is initialized before use */
 	cache_levels = clidr.val;
 	for (i = 0; i < 7; i++)
 		if (((cache_levels >> (i*3)) & 7) == 0)
